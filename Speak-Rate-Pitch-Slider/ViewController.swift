df30b18c77d35ffe5161f620c5a19fd41ee26177
samuraipapa <yury.gitman@gmail.com>
2015-10-13 15:09:39 -0400

    init commit

--- Speak-Rate-Pitch-Slider/ViewController.swift
+++ Speak-Rate-Pitch-Slider/ViewController.swift
@@ -7,9 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //  ViewController Class-Wide Variables
+    let mySpeechSynth = AVSpeechSynthesizer()
+    
+    var myRate: Float = 0.50
+    var myPitch: Float = 0.99
+    var myVolume: Float = 0.50
+    
+    // UI Elements [Outlets]
+
+    @IBOutlet weak var rateNumLabel: UILabel!
+    @IBOutlet weak var pitchNumLabel: UILabel!
+    @IBOutlet weak var volumeNumLabel: UILabel!
+    
+    
+    // UI Elements [Action]
+    
+    @IBAction func rateSliderMoved(sender: UISlider) {
+        rateNumLabel.text =  String(sender.value)
+        myRate = sender.value
+    }
+    
+    @IBAction func pitchSliderMoved(sender: UISlider) {
+        pitchNumLabel.text = String(sender.value)
+        myPitch = sender.value
+    }
+    
+    @IBAction func volumeSliderMoved(sender: UISlider) {
+        volumeNumLabel.text = String(sender.value)
+        myVolume = sender.value
+    }
+    
+    @IBAction func speakButtonPressed(sender: UIButton) {
+    
+        let myUtterance = AVSpeechUtterance(string: "English")
+        myUtterance.rate = myRate
+        myUtterance.pitchMultiplier = Float(myPitch)
+        myUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        mySpeechSynth.speakUtterance(myUtterance)
+  
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
