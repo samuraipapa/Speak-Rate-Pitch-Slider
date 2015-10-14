9f7e7667883f5a083a9a6cb5e2019e08f2b8a1d9
samuraipapa <yury.gitman@gmail.com>
2015-10-14 16:11:48 -0400

    Added BCP-47 Lang Codes
    from:
    https://stackoverflow.com/questions/20590244/what-are-the-bcp-47-voice-c
    odes-available-for-ios-7-avspeechsynthesisvoice/32988309#32988309

--- Speak-Rate-Pitch-Slider/ViewController.swift
+++ Speak-Rate-Pitch-Slider/ViewController.swift
@@ -11,6 +11,7 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    
     //  ViewController Class-Wide Variables
     let mySpeechSynth = AVSpeechSynthesizer()
     
@@ -18,7 +19,50 @@ class ViewController: UIViewController {
     var myPitch: Float = 0.99
     var myVolume: Float = 0.50
     
-    // UI Elements [Outlets]
+    var langCodeAll38 = [("ar-SA","Arabic","Saudi Arabia"),
+    
+    ("cs-CZ", "Czech", "Czech Republic"),
+    ("da-DK", "Danish","Denmark")
+    
+    ]
+/*
+    de-DE       German      Germany
+    el-GR       Modern Greek        Greece
+    en-AU       English     Australia
+    en-GB       English     United Kingdom
+    en-IE       English     Ireland
+    en-US       English     United States
+    en-ZA       English     South Africa
+    es-ES       Spanish     Spain
+    es-MX       Spanish     Mexico
+    fi-FI       Finnish     Finland
+    fr-CA       French      Canada
+    fr-FR       French      France
+    he-IL       Hebrew      Israel
+    hi-IN       Hindi       India
+    hu-HU       Hungarian       Hungary
+    id-ID       Indonesian      Indonesia
+    it-IT       Italian     Italy
+    ja-JP       Japanese        Japan
+    ko-KR       Korean      Republic of Korea
+    nl-BE       Dutch       Belgium
+    nl-NL       Dutch       Netherlands
+    no-NO       Norwegian       Norway
+    pl-PL       Polish      Poland
+    pt-BR       Portuguese      Brazil
+    pt-PT       Portuguese      Portugal
+    ro-RO       Romanian        Romania
+    ru-RU       Russian     Russian Federation
+    sk-SK       Slovak      Slovakia
+    sv-SE       Swedish     Sweden
+    th-TH       Thai        Thailand
+    tr-TR       Turkish     Turkey
+    zh-CN       Chinese     China
+    zh-HK       Chinese     Hong Kong
+    zh-TW       Chinese     Taiwan
+*/
+
+// UI Elements [Outlets]
 
     @IBOutlet weak var rateNumLabel: UILabel!
     @IBOutlet weak var pitchNumLabel: UILabel!
